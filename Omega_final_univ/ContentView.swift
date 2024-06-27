1e0c1d3b059d5395ce6d05d7b70a66001ebee12a
fallenezer <igumnov.danechka@mail.ru>
2024-06-27 22:53:50 +0300

    Initial Commit

---
+++ Omega_final_univ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Omega_final_univ
+//
+//  Created by Даниил Игумнов on 27.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Omega_final_univ/Omega_final_univApp.swift
@@ -0,0 +1,17 @@
+//
+//  Omega_final_univApp.swift
+//  Omega_final_univ
+//
+//  Created by Даниил Игумнов on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Omega_final_univApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
