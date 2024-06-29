3061d1f8021ae54032946302f0a47d4124d72b27
fallenezer <igumnov.danechka@mail.ru>
2024-06-29 11:56:01 +0300

    70%

---
+++ Omega_final_univ/Feedback_country.swift
@@ -0,0 +1,18 @@
+//
+//  Feedback_country.swift
+//  Omega_final_univ
+//
+//  Created by Даниил Игумнов on 29.06.2024.
+//
+
+import SwiftUI
+
+struct Feedback_country: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Feedback_country()
+}
