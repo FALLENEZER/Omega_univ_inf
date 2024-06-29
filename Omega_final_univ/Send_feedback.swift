c496425158c5e1fd6c5d546095206c09340f4a03
fallenezer <igumnov.danechka@mail.ru>
2024-06-29 11:09:41 +0300

    Бомбически реализовано бд

---
+++ Omega_final_univ/Send_feedback.swift
@@ -0,0 +1,18 @@
+//
+//  Send_feedback.swift
+//  Omega_final_univ
+//
+//  Created by Даниил Игумнов on 29.06.2024.
+//
+
+import SwiftUI
+
+struct Send_feedback: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Send_feedback()
+}
